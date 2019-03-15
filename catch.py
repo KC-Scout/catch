87221d945be1284a26ad3c7446e5697e901d0f93
Connor Foster <foster.clee@gmail.com>
2019-03-14 21:27:48 -0500

    Ball image moving correctly

--- catch.py
+++ catch.py
@@ -4,18 +4,19 @@ import sys
 length = 1200
 width = 800
 
+
 screen = pygame.display.set_mode((length, width))
-screen.fill((255, 255, 103))
 screen_rect = screen.get_rect()
 
 ball = pygame.image.load('ball.bmp')
 ball_rect = ball.get_rect()
-
 ball_rect.y = ball_rect.width
 ball_rect.x = screen_rect.centerx
 
 
 while True: 
+    screen.fill((255, 255, 103))
+       
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
