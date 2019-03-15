060ef8417a2bdb44443cae7cc4b22bef685076ba
Connor Foster <foster.clee@gmail.com>
2019-03-14 21:01:56 -0500

    draw ball in center of screen

--- catch.py
+++ catch.py
@@ -1,18 +1,36 @@
 import pygame
 import sys
 
+length = 1200
+width = 800
+
+screen = pygame.display.set_mode((length, width))
+screen.fill((255, 255, 103))
+screen_rect = screen.get_rect()
+
+ball = pygame.image.load('ball.bmp')
+ball_rect = ball.get_rect()
+
+ball_rect.y = ball_rect.width
+ball_rect.x = screen_rect.centerx
+
+
 
 
 while True: 
-    screen = pygame.display.set_mode((1200, 800))
-    screen.fill((255, 255, 103))
+   
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    ball = pygame.image.load('ball.bmp')
-    ball_rect = ball.get_rect()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ball_rect.y -= 10
+        
+
     
-    ball_rect.x = screen.
+    
+    
+    screen.blit(ball, ball_rect)
     
     pygame.display.flip()
