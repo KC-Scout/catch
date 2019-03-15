f3db7e73a821a92b6691b961c9210ae31640c0c5
Connor Foster <foster.clee@gmail.com>
2019-03-14 20:31:41 -0500

    Making yellow background for ball game

--- catch.py
+++ catch.py
@@ -1,1 +1,15 @@
-# This is going to be my first commit for this project.
+import pygame
+import sys
+
+
+
+while True: 
+    screen = pygame.display.set_mode((1200, 800))
+    screen.fill((255, 255, 103))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    ball = pygame.image.load(
+    
+    pygame.display.flip()
