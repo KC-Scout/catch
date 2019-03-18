23ab5d7c9d5d5b5904f892fdd2059c289429d982
Connor Foster <foster.clee@gmail.com>
2019-03-17 20:44:57 -0500

    Modified x to random, reset ball y when gets past bottom of screen

--- catch.py
+++ catch.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from random import randint
+
 
 length = 1200
 width = 800
@@ -11,8 +13,8 @@ screen_rect = screen.get_rect()
 
 ball = pygame.image.load('ball.bmp')
 ball_rect = ball.get_rect()
-ball_rect.y = ball_rect.width
-ball_rect.x = screen_rect.centerx
+ball_rect.y = 15
+ball_rect.x = randint(1, width)
 
 
 while True: 
@@ -21,9 +23,10 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ball_rect.y += 15
     
+    ball_rect.y += 1
+    if ball_rect.y >= width:
+        ball_rect.y = 15
+        ball_rect.x = randint(1, width)
     screen.blit(ball, ball_rect)
     pygame.display.flip()
