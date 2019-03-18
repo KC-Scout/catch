365be600ee962ce882f6b3496fc357d45297c5f5
Connor Foster <foster.clee@gmail.com>
2019-03-17 21:52:12 -0500

    Properly working game of catch

--- catch.py
+++ catch.py
@@ -1,32 +1,55 @@
 import pygame
 import sys
 from random import randint
+from pygame.sprite import Sprite
+
+width = 1200
+height = 800
 
 
-length = 1200
-width = 800
-
-
-screen = pygame.display.set_mode((length, width))
+screen = pygame.display.set_mode((width, height))
 screen_rect = screen.get_rect()
 
+class Ball(Sprite):
+    """Sprite class for ball"""
+    def __init__(self, width):
+        super(Ball, self).__init__()
+        self.image = pygame.image.load('ball.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.y = 15
+        self.rect.x = randint(1, width)
+        
+class Basket(Sprite):
+    """Sprite class for basket"""
+    def __init__(self, screen_rect):
+        super(Basket, self).__init__()
+        self.image = pygame.image.load('basket.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.x = screen_rect.centerx
+        self.rect.bottom = screen_rect.bottom
 
-ball = pygame.image.load('ball.bmp')
-ball_rect = ball.get_rect()
-ball_rect.y = 15
-ball_rect.x = randint(1, width)
-
-
+def collision(sprite_one, sprite_two):
+    return pygame.sprite.collide_rect(sprite_one, sprite_two)
+        
+ball = Ball(width)
+basket = Basket(screen_rect)
 while True: 
     screen.fill((255, 255, 103))
-       
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                basket.rect.x += 40
+            if event.key == pygame.K_LEFT:
+                basket.rect.x -= 40
     
-    ball_rect.y += 1
-    if ball_rect.y >= width:
-        ball_rect.y = 15
-        ball_rect.x = randint(1, width)
-    screen.blit(ball, ball_rect)
+    ball.rect.y += 1
+    if collision(basket, ball) or ball.rect.y >= height:
+        ball.rect.y = 15
+        ball.rect.x = randint(1, width)
+    
+    screen.blit(basket.image, basket.rect)    
+    screen.blit(ball.image, ball.rect)
     pygame.display.flip()
